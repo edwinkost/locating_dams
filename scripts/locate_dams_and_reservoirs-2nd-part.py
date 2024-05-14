c64ceea988d256b2c957fc6eef68ab3b3f4125a9
Edwin Sutanudjaja <sutan101@UU078667-ubuntu>
2024-05-14 10:26:43 +0200

    start testing for linking with hydrolakes etc

--- scripts/locate_dams_and_reservoirs-2nd-part.py
+++ scripts/locate_dams_and_reservoirs-2nd-part.py
@@ -205,11 +205,11 @@ for dam_id in range(1, number_of_dams + 1):
     if dam_id == 1:    
         all_reservoir_extent_ids     = pcr.ifthen(reservoir_extent, pcr.mapmaximum(pcr.spatial(pcr.scalar(dam_id))))
         all_reservoir_surface_area   = pcr.ifthen(reservoir_extent, pcr.mapmaximum(pcr.scalar(reservoir_surface_area)))
-        all_reservoir_fraction_water = pcr.ifthen(reservoir_extent, pcr.mapmaximum(pcr.scalar(all_reservoir_fraction_water))) 
+        all_reservoir_fraction_water = pcr.ifthen(reservoir_extent, pcr.mapmaximum(pcr.scalar(reservoir_fraction_water))) 
     else:
         all_reservoir_extent_ids     = pcr.cover(all_reservoir_extent_ids,     pcr.ifthen(reservoir_extent, pcr.mapmaximum(pcr.spatial(pcr.scalar(dam_id)))))
         all_reservoir_surface_area   = pcr.cover(all_reservoir_surface_area,   pcr.ifthen(reservoir_extent, pcr.mapmaximum(pcr.scalar(reservoir_surface_area))))
-        all_reservoir_fraction_water = pcr.cover(all_reservoir_fraction_water, pcr.ifthen(reservoir_extent, pcr.mapmaximum(pcr.scalar(all_reservoir_fraction_water)))) 
+        all_reservoir_fraction_water = pcr.cover(all_reservoir_fraction_water, pcr.ifthen(reservoir_extent, pcr.mapmaximum(pcr.scalar(reservoir_fraction_water)))) 
 
 
 # ~ # save all_location_corrected_dam_ids to a pcraster map - this will be the location of all dams/outlets
