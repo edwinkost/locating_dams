d39727e57e37ca35a5801b5af47ac8673ebdafcb
valentina <145261580+valebonato@users.noreply.github.com>
2024-05-24 09:56:55 +0200

    adding merging of existing and future dams

---
+++ scripts/merging_existing_future_dams.py
@@ -0,0 +1,93 @@
+import os
+import sys
+import shutil
+
+import pcraster as pcr
+import virtualOS as vos
+
+# pcrglobwb/dynqual files
+# ~ (pcrglobwb_python3) edwinaha@tcn1179.local.snellius.surf.nl:/scratch-shared/edwinaha/dynqual_waterbodies$ ls -lah *.map
+# ~ -rw-r--r--. 1 edwinaha edwinaha 36M May 21 13:15 fracWaterInp_waterBodies5ArcMin_2010.map
+# ~ -rw-r--r--. 1 edwinaha edwinaha 36M May 21 13:15 resMaxCapInp_waterBodies5ArcMin_2010.map
+# ~ -rw-r--r--. 1 edwinaha edwinaha 36M May 21 13:15 resSfAreaInp_waterBodies5ArcMin_2010.map
+# ~ -rw-r--r--. 1 edwinaha edwinaha 36M May 21 13:15 waterBodyIds_waterBodies5ArcMin_2010.map
+# ~ -rw-r--r--. 1 edwinaha edwinaha 36M May 21 13:15 waterBodyTyp_waterBodies5ArcMin_2010.map
+
+# AHA files
+# ~ (pcrglobwb_python3) edwinaha@tcn1179.local.snellius.surf.nl:/home/edwinaha/github/edwinkost/locating_dams/scripts$ ls -lah existing*
+# ~ -rw-r--r--. 1 edwinaha edwinaha 18K May 21 12:55 existing_locate_dams_and_reservoirs_2nd_part.py
+# ~ -rw-r--r--. 1 edwinaha edwinaha 36M May 21 13:06 existing_reservoir_capacity_ids_million_m3.map
+# ~ -rw-r--r--. 1 edwinaha edwinaha 36M May 21 13:06 existing_reservoir_extent_ids.map
+# ~ -rw-r--r--. 1 edwinaha edwinaha 36M May 21 13:06 existing_reservoir_fraction_water_ids.map
+# ~ -rw-r--r--. 1 edwinaha edwinaha 36M May 21 13:06 existing_reservoir_surface_area_ids_m2.map
+
+# IDs based on pcrglowbb
+pgb_ids_file = "/home/edwinaha/github/edwinkost/locating_dams/scripts/merged_waterBodyIds_existing.map"
+pgb_ids = pcr.nominal(pcr.readmap(pgb_ids_file))
+pgb_ids = pcr.ifthen(pcr.scalar(pgb_ids) > 0.0, pgb_ids)
+
+# IDs based on AHA
+aha_ids_file = "/home/edwinaha/github/edwinkost/locating_dams/scripts/future_reservoir_extent_ids.map"
+aha_ids = pcr.nominal(pcr.readmap(aha_ids_file))
+aha_ids = pcr.ifthen(pcr.scalar(aha_ids) > 0.0, aha_ids)
+aha_ids_new_ids_scalar = pcr.mapmaximum(pcr.scalar(pgb_ids)) * 10.0 + pcr.scalar(aha_ids)
+aha_ids = pcr.nominal(aha_ids_new_ids_scalar)
+
+# exclude pgb_ids that are in aha_ids
+pgb_ids_ids_exclusion = pcr.ifthenelse(pcr.defined(aha_ids), pcr.nominal(0), pgb_ids)
+pgb_ids = pcr.nominal(pcr.areaminimum(pcr.scalar(pgb_ids_ids_exclusion), pgb_ids))
+pgb_ids = pcr.ifthen(pcr.scalar(pgb_ids) > 0.0, pgb_ids)
+
+# read all attribute of pcrglobwb water bodies, and exclude the ones identified in AHA
+pgb_fracWaterInp_file = "/home/edwinaha/github/edwinkost/locating_dams/scripts/merged_fracWaterInp_existing.map"
+pgb_resMaxCapInp_file = "/home/edwinaha/github/edwinkost/locating_dams/scripts/merged_resMaxCapInp_existing.map"
+pgb_resSfAreaInp_file = "/home/edwinaha/github/edwinkost/locating_dams/scripts/merged_resSfAreaInp_existing.map"
+pgb_waterBodyIds_file = "home/edwinaha/github/edwinkost/locating_dams/scripts/merged_waterBodyIds_existing.map"
+pgb_waterBodyTyp_file = "home/edwinaha/github/edwinkost/locating_dams/scripts/merged_waterBodyTyp_existing.map"
+pgb_fracWaterInp = pcr.ifthen(pcr.defined(pgb_ids), pcr.readmap(pgb_fracWaterInp_file))
+pgb_resMaxCapInp = pcr.ifthen(pcr.defined(pgb_ids), pcr.readmap(pgb_resMaxCapInp_file))
+pgb_resSfAreaInp = pcr.ifthen(pcr.defined(pgb_ids), pcr.readmap(pgb_resSfAreaInp_file))
+pgb_waterBodyIds = pcr.ifthen(pcr.defined(pgb_ids), pcr.readmap(pgb_waterBodyIds_file))
+pgb_waterBodyTyp = pcr.ifthen(pcr.defined(pgb_ids), pcr.readmap(pgb_waterBodyTyp_file))
+
+
+# read/set AHA properties and merge them to pcrglobwb
+# - ids and extent
+aha_waterBodyIds    = aha_ids
+merged_waterBodyIds = pcr.cover(aha_waterBodyIds, pgb_ids)
+# - fraction water 
+aha_fracWaterInp_file = "/home/edwinaha/github/edwinkost/locating_dams/scripts/future_reservoir_fraction_water_ids.map"
+aha_fracWaterInp      = pcr.readmap(aha_fracWaterInp_file)
+merged_fracWaterInp   = pcr.cover(aha_fracWaterInp, pgb_fracWaterInp)
+# - reservoir capacity
+aha_resMaxCapInp_file = "/home/edwinaha/github/edwinkost/locating_dams/scripts/future_reservoir_capacity_ids_million_m3.map"
+aha_resMaxCapInp      = pcr.readmap(aha_resMaxCapInp_file)
+merged_resMaxCapInp   = pcr.cover(aha_resMaxCapInp, pgb_resMaxCapInp)
+# - reservoir surface area
+aha_resSfAreaInp_file = "/home/edwinaha/github/edwinkost/locating_dams/scripts/future_reservoir_surface_area_ids_m2.map"
+aha_resSfAreaInp      = pcr.readmap(aha_resSfAreaInp_file)
+merged_resSfAreaInp   = pcr.cover(aha_resSfAreaInp, pgb_resSfAreaInp)
+# - reservoir type
+aha_waterBodyTyp      = pcr.ifthen(pcr.defined(aha_ids), pcr.scalar(2.0))
+merged_waterBodyTyp   = pcr.cover(aha_waterBodyTyp, pgb_waterBodyTyp)
+
+# define the landmask based on ldd
+ldd_map_file = "../pcrglobwb_maps/lddsound_05min_version_20210330.map"
+ldd_map = pcr.readmap(ldd_map_file)
+landmask = pcr.defined(ldd_map)
+
+# set the exent of all maps to the landmask only
+merged_waterBodyIds = pcr.ifthen(landmask, pcr.cover(merged_waterBodyIds, pcr.nominal(0)))
+merged_fracWaterInp = pcr.ifthen(landmask, pcr.cover(merged_fracWaterInp, 0.0))
+merged_resMaxCapInp = pcr.ifthen(landmask, pcr.cover(merged_resMaxCapInp, 0.0))
+merged_resSfAreaInp = pcr.ifthen(landmask, pcr.cover(merged_resSfAreaInp, 0.0))
+merged_waterBodyTyp = pcr.ifthen(landmask, pcr.cover(merged_waterBodyTyp, 0.0))
+
+# save all reservoir types
+pcr.report(merged_waterBodyIds, "merged_waterBodyIds_existing_future.map")
+pcr.report(merged_fracWaterInp, "merged_fracWaterInp_existing_future.map")
+pcr.report(merged_resMaxCapInp, "merged_resMaxCapInp_existing_future.map")
+pcr.report(merged_resSfAreaInp, "merged_resSfAreaInp_existing_future.map")
+pcr.report(merged_waterBodyTyp, "merged_waterBodyTyp_existing_future.map")
+
+
